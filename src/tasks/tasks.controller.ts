9031c1bdc007098e4c810aaa1d82488ec029eaa9
Lakshan Costa <lakshanrcosta@gmail.com>
2025-07-23 01:00:57 +0300

    feat: enhance TasksController with task retrieval and creation methods

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,14 +1,35 @@
-import { Controller, Get, Param } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  NotFoundException,
+  Param,
+  Post,
+} from '@nestjs/common';
+import { ITask } from './task.model';
+import { TasksService } from './tasks.service';
+import { CreateTaskDto } from './create-task.dto';
 
 @Controller('tasks')
 export class TasksController {
+  constructor(private readonly tasksService: TasksService) {}
+
   @Get()
-  public findAll(): string[] {
-    return ['Task 1', 'Task 2', 'Task 3'];
+  public findAll(): ITask[] {
+    return this.tasksService.findAll();
   }
 
   @Get('/:id')
-  public findOne(@Param('id') id: number): string {
-    return `Task ${id}`;
+  public findOne(@Param('id') id: string): ITask {
+    const task = this.tasksService.findOne(id);
+    if (!task) {
+      throw new NotFoundException(`Task with ID ${id} not found`);
+    }
+    return task;
+  }
+
+  @Post()
+  public create(@Body() createTaskDto: CreateTaskDto): ITask {
+    return this.tasksService.createTask(createTaskDto);
   }
 }
